90990459b57b5126a0380c9e839655a42c45ee5e
Neha <66562688+Nehachoudhary19@users.noreply.github.com>
2020-12-25 13:45:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
-  var byeSpeaker{};
-  var helloSpeaker{};
+  var byeSpeaker={};
+  var helloSpeaker={};
 
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
